cb93a1a56c719f5feeabbb81d4c789b957c0f884
IbrahimQassem <26601381+IbrahimQassem@users.noreply.github.com>
2026-09-14 02:24:55 +0300

    chore(release): bump version to 3.0.0+30 for Google Play release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ val keystoreProperties = Properties().apply {
 android {
     namespace = "com.sanaadev.hudhudfm"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = flutter.ndkVersion
 
     buildFeatures {
         resValues = true
