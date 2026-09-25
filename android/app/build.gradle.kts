851ea9ac04c6c53fc2320de3ddaec69cf8a70d64
IbrahimQassem <26601381+IbrahimQassem@users.noreply.github.com>
2026-09-25 20:15:54 +0300

    chore: update compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ val keystoreProperties = Properties().apply {
 
 android {
     namespace = "com.sana.dev.fm"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     buildFeatures {
@@ -50,7 +50,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         resValue(
